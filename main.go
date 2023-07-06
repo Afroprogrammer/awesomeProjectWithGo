08f0100313ba597e6951d68c4d1a921d043a0745
franklin <forsonfranklin@gmail.com>
2023-07-06 16:34:25 -0300

    added a method for searching a particular page

--- main.go
+++ main.go
@@ -1,17 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"github.com/joho/godotenv"
+	"html/template"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 )
 
+var tpl = template.Must(template.ParseFiles("index.html"))
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1> Hello World<h1>"))
+	tpl.Execute(w, nil)
 }
 
+// method below used for search
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	u, err := url.Parse(r.URL.String())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	params := u.Query()
+	searchQuery := params.Get("q")
+	page := params.Get("page")
+	if page == "" {
+		page = "1"
+	}
+	fmt.Println("Search Query is: ", searchQuery)
+	fmt.Println("Page is: ", page)
+}
 func main() {
+	//reading port from env file
 	err := godotenv.Load()
 	if err != nil {
 		log.Println("Error loading .env file")
@@ -20,7 +42,10 @@ func main() {
 	if port == "" {
 		port = "3000"
 	}
+	fs := http.FileServer(http.Dir("assets"))
+
 	mux := http.NewServeMux()
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
 	mux.HandleFunc("/", indexHandler)
 	http.ListenAndServe(":"+port, mux)
 }
